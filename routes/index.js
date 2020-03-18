26abb8191fc511fef5ce6d7416582cd168aff474
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-03-18 13:41:11 -0400

    Added form to add new messages

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 let messages = [
   {
-    user: 'Frank',
-    message: "testing 1 2 3"
+    user: "Frank",
+    message: "testing 1 2 3",
+    time: new Date()
   },
   {
-    user: 'John',
-    message: "still testing lol"
+    user: "John",
+    message: "still testing lol",
+    time: new Date()
   },
   {
-    user: 'Gizmo',
-    message: "Old boy"
+    user: "Gizmo",
+    message: "Old boy",
+    time: new Date()
   }
-]
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Messageboard", messages: messages });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
+
+router.get("/new", function(req, res) {
+  res.render("form", { title: "Input Form" });
+});
+
+router.post("/new", function(req, res){
+  let author = req.body.author;
+  let msg = req.body.msg;
+  messages.push({user: author, message: msg, time: new Date()});
+  res.redirect('/')
+})
 
 module.exports = router;

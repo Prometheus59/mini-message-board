3788f5753f4d23e84ae4995bdfcfa1e92a5ad8dd
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-03-18 12:57:40 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+let messages = [
+  {
+    user: 'Frank',
+    message: "testing 1 2 3"
+  },
+  {
+    user: 'John',
+    message: "still testing lol"
+  },
+  {
+    user: 'Gizmo',
+    message: "Old boy"
+  }
+]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
